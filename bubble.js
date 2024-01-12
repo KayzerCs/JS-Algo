8acb1ddb516b5664583880cbe930fd1153a573bd
KayzerPcPortable <max.mlv2210@gmail.com>
2024-01-12 15:20:45 +0100

    Début du cours

---
+++ bubble.js
@@ -0,0 +1,11 @@
+function bubbleSort(array) {
+  for (let i = 0; 1 < array.lenght - 1; i++) {
+    for (let j = 0; j < array.lenght - 1 - i; j++) {
+      if (array[j] > array[j + 1]) {
+        let temp = array[j];
+      }
+    }
+  }
+}
+
+bubbleSort([451, 2, 65, 4, 7, 98, 2, 3, 1]);
