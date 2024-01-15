263cd0e9690366c76cbf86b029c06748550b5b2b
Kayzer <max.mlv2210@gmail.com>
2024-01-15 13:26:45 +0100

    BubbleSort

--- bubble.js
+++ bubble.js
@@ -1,11 +1,15 @@
 function bubbleSort(array) {
-  for (let i = 0; 1 < array.lenght - 1; i++) {
-    for (let j = 0; j < array.lenght - 1 - i; j++) {
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = 0; j < array.length - 1 - i; j++) {
       if (array[j] > array[j + 1]) {
-        let temp = array[j];
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
       }
     }
   }
 }
+
+// function bubbleSort(array) {
+//   console.log(array.sort((a, b) => a - b));
+// }
 
 bubbleSort([451, 2, 65, 4, 7, 98, 2, 3, 1]);
